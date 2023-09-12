e7e756479a3e4f7fc78f857d6c948e0c6944f9f7
e48mils <sumil96@hotmail.com>
2023-09-12 12:59:01 +0200

    removed build folder from git

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/cpp_projects/cpp_onnx_proj
-# Build directory: C:/cpp_projects/cpp_onnx_proj/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
